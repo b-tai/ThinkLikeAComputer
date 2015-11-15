3411d0decfb988484616a5e65818f6ce32a61cac
danwang1995 <danwang1995@gmail.com>
2015-11-14 19:23:00 -0500

    Querying Clarifai to score guesses

---
+++ expressapp/WebRole1/bin/download.ps1
@@ -0,0 +1,233 @@
+$runtimeUrl = $args[0]
+$overrideUrl = $args[1]
+$current = [string] (Get-Location -PSProvider FileSystem)
+$client = New-Object System.Net.WebClient
+
+function downloadWithRetry {
+	param([string]$url, [string]$dest, [int]$retry) 
+	Write-Host
+	Write-Host "Attempt: $retry"
+    Write-Host
+    trap {
+    	Write-Host $_.Exception.ToString()
+	    if ($retry -lt 5) {
+	    	$retry=$retry+1
+	    	Write-Host
+	    	Write-Host "Waiting 5 seconds and retrying"
+	    	Write-Host
+	    	Start-Sleep -s 5
+	    	downloadWithRetry $url $dest $retry $client
+	    }
+	    else {
+	    	Write-Host "Download failed"
+	   		throw "Max number of retries downloading [5] exceeded" 	
+	    }
+    }
+    $client.downloadfile($url, $dest)
+}
+
+function download($url, $dest) {
+	Write-Host "Downloading $url"
+	downloadWithRetry $url $dest 1
+}
+
+function copyOnVerify($file, $output) {
+  Write-Host "Verifying $file"
+  $verify = Get-AuthenticodeSignature $file
+  Out-Host -InputObject $verify
+  if ($verify.Status -ne "Valid") {
+     throw "Invalid signature for runtime package $file"
+  }
+  else {
+    mv $file $output
+  }
+}
+
+if ($overrideUrl) {
+    Write-Host "Using override url: $overrideUrl"
+	$url = $overrideUrl
+}
+else {
+	$url = $runtimeUrl
+}
+
+foreach($singleUrl in $url -split ";") 
+{
+    $suffix = Get-Random
+    $downloaddir = $current + "\sandbox" + $suffix
+    mkdir $downloaddir
+    $dest = $downloaddir + "\sandbox.exe"
+    download $singleUrl $dest
+    $final = $downloaddir + "\runtime.exe"
+    copyOnVerify $dest $final
+    if (Test-Path -LiteralPath $final)
+    {
+      cd $downloaddir
+      if ($host.Version.Major -eq 3)
+      {
+        .\runtime.exe -y | Out-Null
+        .\setup.cmd
+      }
+      else
+      {
+        Start-Process -FilePath $final -ArgumentList -y -Wait 
+        $cmd = $downloaddir + "\setup.cmd"
+        Start-Process -FilePath $cmd -Wait
+      }
+    }
+    else
+    {
+      throw "Unable to verify package"
+    }
+    cd $current
+    if (Test-Path -LiteralPath $downloaddir)
+    {
+        Remove-Item -LiteralPath $downloaddir -Force -Recurse
+    }
+}
+
+# SIG # Begin signature block
+# MIIavwYJKoZIhvcNAQcCoIIasDCCGqwCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUcSvO7X5VZ1CI9iBTAfMAGZQm
+# g6igghWCMIIEwzCCA6ugAwIBAgITMwAAAI6oGdc/Not6QAAAAAAAjjANBgkqhkiG
+# 9w0BAQUFADB3MQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4G
+# A1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSEw
+# HwYDVQQDExhNaWNyb3NvZnQgVGltZS1TdGFtcCBQQ0EwHhcNMTUxMDA3MTgxNDA0
+# WhcNMTcwMTA3MTgxNDA0WjCBszELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hp
+# bmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jw
+# b3JhdGlvbjENMAsGA1UECxMETU9QUjEnMCUGA1UECxMebkNpcGhlciBEU0UgRVNO
+# OkMwRjQtMzA4Ni1ERUY4MSUwIwYDVQQDExxNaWNyb3NvZnQgVGltZS1TdGFtcCBT
+# ZXJ2aWNlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAywdgwKLb/d5m
+# CuMDaaLIcQmqzWwyzYAUEkBQ4rpK3eMmWYUXXk1dO3cce+a2pVxLXzbfGoU495Ur
+# exStkrXJxdkCtBvX8FMYoVHTXdJy7/cxP8WKKCndp/mTOW5Q1cjKQugwULpgOEwv
+# TMxZcHTdDzOG+dF36UB76OOreOkqHNuWnMwCa9/BDSobefB4jaFDXFyLz6hL+XSY
+# M4OQ9DSiNDg2ltZPCtiVP2Jo/kGDcfL1hKA+7tBJxo6nQ9DN22ctU9yyKIIcHqfe
+# hMYTX6DF6Lc1sDkg9rXlSuP8MNuBDPSTThiAYa2i8JkE4GRErsBpZAg1PxyEJKHI
+# 8Fr07MMDjQIDAQABo4IBCTCCAQUwHQYDVR0OBBYEFKlX2YHJ9sQutVnd1XXHzN+p
+# wwSvMB8GA1UdIwQYMBaAFCM0+NlSRnAK7UD7dvuzK7DDNbMPMFQGA1UdHwRNMEsw
+# SaBHoEWGQ2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1Y3Rz
+# L01pY3Jvc29mdFRpbWVTdGFtcFBDQS5jcmwwWAYIKwYBBQUHAQEETDBKMEgGCCsG
+# AQUFBzAChjxodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL2NlcnRzL01pY3Jv
+# c29mdFRpbWVTdGFtcFBDQS5jcnQwEwYDVR0lBAwwCgYIKwYBBQUHAwgwDQYJKoZI
+# hvcNAQEFBQADggEBAGFdFKhKq49tsj3Yg+54ycag/iq1dfa3sLF0D2s1cdF57XjN
+# 6KSOz4QzqGOsTkP6DL8tH7IHJvbphr6KtzPBpRA8VfBXjlxG03WtohgaWP0mi7oA
+# fb+6akTbZPaKkdd7VurTfjyJmHM1g3d2QkoHKvHmKp5GZLB1ZOfWuxEyMb6xK5S+
+# Fab6nKbrbqwzoTSmLITYQwfallq8aNIm+k5tsgTgYqdytfuUXULdw/Uw8p56DFRk
+# oK4TUetV/i1W08zjGE4tp2wV2DHhNaUPHnYHYarq+RzQELs3OQtkFA1zO8Igx01z
+# kdxVHOWTPL/nRXDE/j+nGeMaprWA/qN21+dpeAgwggTsMIID1KADAgECAhMzAAAB
+# Cix5rtd5e6asAAEAAAEKMA0GCSqGSIb3DQEBBQUAMHkxCzAJBgNVBAYTAlVTMRMw
+# EQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25kMR4wHAYDVQQKExVN
+# aWNyb3NvZnQgQ29ycG9yYXRpb24xIzAhBgNVBAMTGk1pY3Jvc29mdCBDb2RlIFNp
+# Z25pbmcgUENBMB4XDTE1MDYwNDE3NDI0NVoXDTE2MDkwNDE3NDI0NVowgYMxCzAJ
+# BgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25k
+# MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xDTALBgNVBAsTBE1PUFIx
+# HjAcBgNVBAMTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjCCASIwDQYJKoZIhvcNAQEB
+# BQADggEPADCCAQoCggEBAJL8bza74QO5KNZG0aJhuqVG+2MWPi75R9LH7O3HmbEm
+# UXW92swPBhQRpGwZnsBfTVSJ5E1Q2I3NoWGldxOaHKftDXT3p1Z56Cj3U9KxemPg
+# 9ZSXt+zZR/hsPfMliLO8CsUEp458hUh2HGFGqhnEemKLwcI1qvtYb8VjC5NJMIEb
+# e99/fE+0R21feByvtveWE1LvudFNOeVz3khOPBSqlw05zItR4VzRO/COZ+owYKlN
+# Wp1DvdsjusAP10sQnZxN8FGihKrknKc91qPvChhIqPqxTqWYDku/8BTzAMiwSNZb
+# /jjXiREtBbpDAk8iAJYlrX01boRoqyAYOCj+HKIQsaUCAwEAAaOCAWAwggFcMBMG
+# A1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBSJ/gox6ibN5m3HkZG5lIyiGGE3
+# NDBRBgNVHREESjBIpEYwRDENMAsGA1UECxMETU9QUjEzMDEGA1UEBRMqMzE1OTUr
+# MDQwNzkzNTAtMTZmYS00YzYwLWI2YmYtOWQyYjFjZDA1OTg0MB8GA1UdIwQYMBaA
+# FMsR6MrStBZYAck3LjMWFrlMmgofMFYGA1UdHwRPME0wS6BJoEeGRWh0dHA6Ly9j
+# cmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1Y3RzL01pY0NvZFNpZ1BDQV8w
+# OC0zMS0yMDEwLmNybDBaBggrBgEFBQcBAQROMEwwSgYIKwYBBQUHMAKGPmh0dHA6
+# Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kvY2VydHMvTWljQ29kU2lnUENBXzA4LTMx
+# LTIwMTAuY3J0MA0GCSqGSIb3DQEBBQUAA4IBAQCmqFOR3zsB/mFdBlrrZvAM2PfZ
+# hNMAUQ4Q0aTRFyjnjDM4K9hDxgOLdeszkvSp4mf9AtulHU5DRV0bSePgTxbwfo/w
+# iBHKgq2k+6apX/WXYMh7xL98m2ntH4LB8c2OeEti9dcNHNdTEtaWUu81vRmOoECT
+# oQqlLRacwkZ0COvb9NilSTZUEhFVA7N7FvtH/vto/MBFXOI/Enkzou+Cxd5AGQfu
+# FcUKm1kFQanQl56BngNb/ErjGi4FrFBHL4z6edgeIPgF+ylrGBT6cgS3C6eaZOwR
+# XU9FSY0pGi370LYJU180lOAWxLnqczXoV+/h6xbDGMcGszvPYYTitkSJlKOGMIIF
+# vDCCA6SgAwIBAgIKYTMmGgAAAAAAMTANBgkqhkiG9w0BAQUFADBfMRMwEQYKCZIm
+# iZPyLGQBGRYDY29tMRkwFwYKCZImiZPyLGQBGRYJbWljcm9zb2Z0MS0wKwYDVQQD
+# EyRNaWNyb3NvZnQgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkwHhcNMTAwODMx
+# MjIxOTMyWhcNMjAwODMxMjIyOTMyWjB5MQswCQYDVQQGEwJVUzETMBEGA1UECBMK
+# V2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0
+# IENvcnBvcmF0aW9uMSMwIQYDVQQDExpNaWNyb3NvZnQgQ29kZSBTaWduaW5nIFBD
+# QTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJyWVwZMGS/HZpgICBC
+# mXZTbD4b1m/My/Hqa/6XFhDg3zp0gxq3L6Ay7P/ewkJOI9VyANs1VwqJyq4gSfTw
+# aKxNS42lvXlLcZtHB9r9Jd+ddYjPqnNEf9eB2/O98jakyVxF3K+tPeAoaJcap6Vy
+# c1bxF5Tk/TWUcqDWdl8ed0WDhTgW0HNbBbpnUo2lsmkv2hkL/pJ0KeJ2L1TdFDBZ
+# +NKNYv3LyV9GMVC5JxPkQDDPcikQKCLHN049oDI9kM2hOAaFXE5WgigqBTK3S9dP
+# Y+fSLWLxRT3nrAgA9kahntFbjCZT6HqqSvJGzzc8OJ60d1ylF56NyxGPVjzBrAlf
+# A9MCAwEAAaOCAV4wggFaMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFMsR6MrS
+# tBZYAck3LjMWFrlMmgofMAsGA1UdDwQEAwIBhjASBgkrBgEEAYI3FQEEBQIDAQAB
+# MCMGCSsGAQQBgjcVAgQWBBT90TFO0yaKleGYYDuoMW+mPLzYLTAZBgkrBgEEAYI3
+# FAIEDB4KAFMAdQBiAEMAQTAfBgNVHSMEGDAWgBQOrIJgQFYnl+UlE/wq4QpTlVnk
+# pDBQBgNVHR8ESTBHMEWgQ6BBhj9odHRwOi8vY3JsLm1pY3Jvc29mdC5jb20vcGtp
+# L2NybC9wcm9kdWN0cy9taWNyb3NvZnRyb290Y2VydC5jcmwwVAYIKwYBBQUHAQEE
+# SDBGMEQGCCsGAQUFBzAChjhodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL2Nl
+# cnRzL01pY3Jvc29mdFJvb3RDZXJ0LmNydDANBgkqhkiG9w0BAQUFAAOCAgEAWTk+
+# fyZGr+tvQLEytWrrDi9uqEn361917Uw7LddDrQv+y+ktMaMjzHxQmIAhXaw9L0y6
+# oqhWnONwu7i0+Hm1SXL3PupBf8rhDBdpy6WcIC36C1DEVs0t40rSvHDnqA2iA6VW
+# 4LiKS1fylUKc8fPv7uOGHzQ8uFaa8FMjhSqkghyT4pQHHfLiTviMocroE6WRTsgb
+# 0o9ylSpxbZsa+BzwU9ZnzCL/XB3Nooy9J7J5Y1ZEolHN+emjWFbdmwJFRC9f9Nqu
+# 1IIybvyklRPk62nnqaIsvsgrEA5ljpnb9aL6EiYJZTiU8XofSrvR4Vbo0HiWGFzJ
+# NRZf3ZMdSY4tvq00RBzuEBUaAF3dNVshzpjHCe6FDoxPbQ4TTj18KUicctHzbMrB
+# 7HCjV5JXfZSNoBtIA1r3z6NnCnSlNu0tLxfI5nI3EvRvsTxngvlSso0zFmUeDord
+# EN5k9G/ORtTTF+l5xAS00/ss3x+KnqwK+xMnQK3k+eGpf0a7B2BHZWBATrBC7E7t
+# s3Z52Ao0CW0cgDEf4g5U3eWh++VHEK1kmP9QFi58vwUheuKVQSdpw5OPlcmN2Jsh
+# rg1cnPCiroZogwxqLbt2awAdlq3yFnv2FoMkuYjPaqhHMS+a3ONxPdcAfmJH0c6I
+# ybgY+g5yjcGjPa8CQGr/aZuW4hCoELQ3UAjWwz0wggYHMIID76ADAgECAgphFmg0
+# AAAAAAAcMA0GCSqGSIb3DQEBBQUAMF8xEzARBgoJkiaJk/IsZAEZFgNjb20xGTAX
+# BgoJkiaJk/IsZAEZFgltaWNyb3NvZnQxLTArBgNVBAMTJE1pY3Jvc29mdCBSb290
+# IENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0wNzA0MDMxMjUzMDlaFw0yMTA0MDMx
+# MzAzMDlaMHcxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xITAf
+# BgNVBAMTGE1pY3Jvc29mdCBUaW1lLVN0YW1wIFBDQTCCASIwDQYJKoZIhvcNAQEB
+# BQADggEPADCCAQoCggEBAJ+hbLHf20iSKnxrLhnhveLjxZlRI1Ctzt0YTiQP7tGn
+# 0UytdDAgEesH1VSVFUmUG0KSrphcMCbaAGvoe73siQcP9w4EmPCJzB/LMySHnfL0
+# Zxws/HvniB3q506jocEjU8qN+kXPCdBer9CwQgSi+aZsk2fXKNxGU7CG0OUoRi4n
+# rIZPVVIM5AMs+2qQkDBuh/NZMJ36ftaXs+ghl3740hPzCLdTbVK0RZCfSABKR2YR
+# JylmqJfk0waBSqL5hKcRRxQJgp+E7VV4/gGaHVAIhQAQMEbtt94jRrvELVSfrx54
+# QTF3zJvfO4OToWECtR0Nsfz3m7IBziJLVP/5BcPCIAsCAwEAAaOCAaswggGnMA8G
+# A1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFCM0+NlSRnAK7UD7dvuzK7DDNbMPMAsG
+# A1UdDwQEAwIBhjAQBgkrBgEEAYI3FQEEAwIBADCBmAYDVR0jBIGQMIGNgBQOrIJg
+# QFYnl+UlE/wq4QpTlVnkpKFjpGEwXzETMBEGCgmSJomT8ixkARkWA2NvbTEZMBcG
+# CgmSJomT8ixkARkWCW1pY3Jvc29mdDEtMCsGA1UEAxMkTWljcm9zb2Z0IFJvb3Qg
+# Q2VydGlmaWNhdGUgQXV0aG9yaXR5ghB5rRahSqClrUxzWPQHEy5lMFAGA1UdHwRJ
+# MEcwRaBDoEGGP2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1
+# Y3RzL21pY3Jvc29mdHJvb3RjZXJ0LmNybDBUBggrBgEFBQcBAQRIMEYwRAYIKwYB
+# BQUHMAKGOGh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kvY2VydHMvTWljcm9z
+# b2Z0Um9vdENlcnQuY3J0MBMGA1UdJQQMMAoGCCsGAQUFBwMIMA0GCSqGSIb3DQEB
+# BQUAA4ICAQAQl4rDXANENt3ptK132855UU0BsS50cVttDBOrzr57j7gu1BKijG1i
+# uFcCy04gE1CZ3XpA4le7r1iaHOEdAYasu3jyi9DsOwHu4r6PCgXIjUji8FMV3U+r
+# kuTnjWrVgMHmlPIGL4UD6ZEqJCJw+/b85HiZLg33B+JwvBhOnY5rCnKVuKE5nGct
+# xVEO6mJcPxaYiyA/4gcaMvnMMUp2MT0rcgvI6nA9/4UKE9/CCmGO8Ne4F+tOi3/F
+# NSteo7/rvH0LQnvUU3Ih7jDKu3hlXFsBFwoUDtLaFJj1PLlmWLMtL+f5hYbMUVbo
+# nXCUbKw5TNT2eb+qGHpiKe+imyk0BncaYsk9Hm0fgvALxyy7z0Oz5fnsfbXjpKh0
+# NbhOxXEjEiZ2CzxSjHFaRkMUvLOzsE1nyJ9C/4B5IYCeFTBm6EISXhrIniIh0EPp
+# K+m79EjMLNTYMoBMJipIJF9a6lbvpt6Znco6b72BJ3QGEe52Ib+bgsEnVLaxaj2J
+# oXZhtG6hE6a/qkfwEm/9ijJssv7fUciMI8lmvZ0dhxJkAj0tr1mPuOQh5bWwymO0
+# eFQF1EEuUKyUsKV4q7OglnUa2ZKHE3UiLzKoCG6gW4wlv6DvhMoh1useT8ma7kng
+# 9wFlb4kLfchpyOZu6qeXzjEp/w7FW1zYTRuh2Povnj8uVRZryROj/TGCBKcwggSj
+# AgEBMIGQMHkxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xIzAh
+# BgNVBAMTGk1pY3Jvc29mdCBDb2RlIFNpZ25pbmcgUENBAhMzAAABCix5rtd5e6as
+# AAEAAAEKMAkGBSsOAwIaBQCggcAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQw
+# HAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFIe9
+# aJql62ihni8/lctUNsAntO+CMGAGCisGAQQBgjcCAQwxUjBQoDaANABNAGkAYwBy
+# AG8AcwBvAGYAdAAgAEEAegB1AHIAZQAgAFAAbwB3AGUAcgBTAGgAZQBsAGyhFoAU
+# aHR0cDovL0NvZGVTaWduSW5mbyAwDQYJKoZIhvcNAQEBBQAEggEAEUNj+tqMi5Vy
+# sRUjS4BhgIpLKCu4ZVFTp2luf3bB8iJL9wyvReB7ufwimFW3Fk6dIm83PIt/AaLg
+# Otgv4rPxciIQUDT2dUwiEd1tAlEAJwy8nYsvg4+gD+f0uVAqEMFmrZDoBLD2GL55
+# wyNnCHwLCezO4DHnyG2j8agNobQn25RWyRu0PLS+UjBJqfK5ObIRRnauox6dPYbv
+# v2MkC/A7Mh478FkFjfom7gOEdlsjaTuRp9KZz+MpM8VVcqgU827HtanjFe0AWofr
+# 5Eg7TO7xm2/eGAlrJqJNN3vrJcuAO+TNlZqyjvN41M//fAf5oXTJporwP651xTDr
+# Y0ddAnQpmqGCAigwggIkBgkqhkiG9w0BCQYxggIVMIICEQIBATCBjjB3MQswCQYD
+# VQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEe
+# MBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSEwHwYDVQQDExhNaWNyb3Nv
+# ZnQgVGltZS1TdGFtcCBQQ0ECEzMAAACOqBnXPzaLekAAAAAAAI4wCQYFKw4DAhoF
+# AKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1
+# MTEwNzAxMjUyOVowIwYJKoZIhvcNAQkEMRYEFLvDL2yL3NprRBxbB+HZMgKKc89L
+# MA0GCSqGSIb3DQEBBQUABIIBAATOLtdphTGMzsJeUtu8o7nT7z94OU3hdyGCx8Ag
+# aHxJ1fIMyQNBZAYaVp5srcGdSIcKtHfH53cWnzTwZsQIzL6yxVXzuWexBqRwzPVh
+# jFj4Tq3Q9jxQoiNsy6vWqACSpeEFxi2ibhuDnyQPGNpTrGJu6maQYyWdi30503i4
+# uRDt0n9r+4+q8cz0BG9WJ8vq/9DoLaVkGBAKCzVjrtjQqJNR8xJz4PgQVDTUwEu7
+# 9KOY5M0MFqVzgYixDBO1ZXqqDfSj4ly8J67BsmWoBY2odpF/UzrldE8BW0ZiEnqZ
+# aVbUvdVqFaUgPWrLtxq9aWJGRbXWUEJD0d2H3nDkw9EuSTw=
+# SIG # End signature block
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var display = "";
 var Clarifai = require('./clarifai_node.js');
 var clientID = "Ujn2c65_Y-9-FyZRNDg5GTew2n6g5apT1DGh8BX-";
 var clientSecret = "DoOB5xDgVAcZqxuL1M2kXDaulw_sw6LwzY_Qcip8";
+var tagsToScores = {};
+var scoreOfLatestGuess = 0;
 Clarifai.initAPI(clientID, clientSecret);
 
 // Setting a throttle handler lets you know when the service is unavailable because of throttling. It will let
@@ -14,24 +16,20 @@ Clarifai.setThrottleHandler( function( bThrottled, waitSeconds ) {
 });
 
 function commonResultHandler( err, res ) {
-	if( err != null ) {
-		if( typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
-			console.log("TAG request timed out");
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
-			console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");				
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
-			console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");				
-		}
-		else if( typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
-			console.log("Clarifai host is throttling this application.");				
-		}
-		else {
-			console.log("TAG request encountered an unexpected error: ");
-			console.log(err);				
-		}
-	}
+	if (err != null) {
+        if (typeof err["status_code"] === "string" && err["status_code"] === "TIMEOUT") {
+            console.log("TAG request timed out");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "ALL_ERROR") {
+            console.log("TAG request received ALL_ERROR. Contact Clarifai support if it continues.");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "TOKEN_FAILURE") {
+            console.log("TAG request received TOKEN_FAILURE. Contact Clarifai support if it continues.");
+        } else if (typeof err["status_code"] === "string" && err["status_code"] === "ERROR_THROTTLED") {
+            console.log("Clarifai host is throttling this application.");
+        } else {
+            console.log("TAG request encountered an unexpected error: ");
+            console.log(err);
+        }
+    }
 	else {
 			// if some images were successfully tagged and some encountered errors,
 			// the status_code PARTIAL_ERROR is returned. In this case, we inspect the
@@ -44,29 +42,52 @@ function commonResultHandler( err, res ) {
 				// the request completed successfully
 				for( i = 0; i < res.results.length; i++ ) {
 					if( res["results"][i]["status_code"] === "OK" ) {
-						display = 'docid='+res.results[i].docid +
-							' local_id='+res.results[i].local_id +
-							' tags='+res["results"][i].result["tag"]["classes"];
-						console.log( 'docid='+res.results[i].docid +
-							' local_id='+res.results[i].local_id +
-							' tags='+res["results"][i].result["tag"]["classes"] )
+						var numTags = res["results"][i].result["tag"]["classes"].length;
+						var sumOfInverseUnideductedProbabilities = 0;
+
+						for (j = 0; j < numTags; j++) {
+							sumOfInverseUnideductedProbabilities += 1 / (1 - res["results"][i].result["tag"]["probs"][j]);
+						}
+
+						for (j = 0; j < numTags; j++) {
+							var score = Math.round(100 / (1 - res["results"][i].result["tag"]["probs"][j]) / sumOfInverseUnideductedProbabilities);
+
+							if (score >= 5) {
+								tagsToScores[res["results"][i].result["tag"]["classes"][j]] = score;
+							}
+						}
+
+						console.log(tagsToScores);
 					}
 					else {
 						console.log( 'docid='+res.results[i].docid +
 							' local_id='+res.results[i].local_id + 
 							' status_code='+res.results[i].status_code +
-							' error = '+res.results[i]["result"]["error"] )
+							' error = '+res.results[i]["result"]["error"] );
 					}
 				}
+
+				getScoreOfGuess("cat");
+				getScoreOfGuess("nonsenseXYZ"); // expect 0
 
 			}		
 	}
 	callback();
 }
 
+function getScoreOfGuess(word) {
+	scoreOfLatestGuess = tagsToScores[word];
+
+	if (scoreOfLatestGuess === undefined) {
+		scoreOfLatestGuess = 0;
+	}
+
+	console.log(scoreOfLatestGuess);
+}
+
 // exampleTagSingleURL() shows how to request the tags for a single image URL
 function exampleTagSingleURL() {
-	var testImageURL = 'http://www.clarifai.com/img/metro-north.jpg';
+	var testImageURL = 'https://lh3.googleusercontent.com/-xlXLYPjhRKE/VH-pLDYTXpI/AAAAAAAAIY8/5WpXe-gfY2I/w640-h360/white-cat-blue-eyes-640x360.jpg';
 	var ourId = "train station 1"; // this is any string that identifies the image to your system
 
 	//Clarifai.setRequestTimeout( 100 ); // in ms - expect: ensure no timeout 
@@ -76,6 +97,7 @@ function exampleTagSingleURL() {
 
 
 exampleTagSingleURL();
+
 function callback() {
 
 	var http = require('http')
@@ -89,7 +111,9 @@ function callback() {
 
 
 // images:
-// "http://www.clarifai.com/img/metro-north.jpg";
-// http://i.imgur.com/mGafnMa.png
-// http://gallery.photo.net/photo/3204336-lg.jpg
-// http://gallery.photo.net/photo/3204337-lg.jpg
+// "http://www.clarifai.com/img/metro-north.jpg"; --- train station
+// http://i.imgur.com/mGafnMa.png --- collage
+// http://gallery.photo.net/photo/3204336-lg.jpg --- dark room (not a very good image for this purpose)
+// http://gallery.photo.net/photo/3204337-lg.jpg --- owl
+// https://lh3.googleusercontent.com/-xlXLYPjhRKE/VH-pLDYTXpI/AAAAAAAAIY8/5WpXe-gfY2I/w640-h360/white-cat-blue-eyes-640x360.jpg --- cat
+// http://vapable.com/wp-content/uploads/2013/08/raspberry-flavour-e-liquid.jpg --- raspberries
